213d9145fb71f05883a3ef29fbf6d190faa68e37
Eris <erisbrabham@gmail.com>
2024-03-17 21:56:52 +0000

    Build list objects

--- scheme_interpreter/src/lib.rs
+++ scheme_interpreter/src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(let_chains)]
 
 use std::{
-    collections::HashMap, error::Error, sync::{Arc, Mutex}
+    collections::HashMap,
+    error::Error,
+    sync::{Arc, Mutex},
 };
 
 use frame::Frame;
@@ -30,15 +32,38 @@ impl InterpreterContext {
 
     pub fn interpret(&mut self, ast: AST) -> InterpreterResult<()> {
         match ast {
-            AST::Operation(op, params) => self.interpret_operation(*op, params),
+            AST::Operation(op, params) => return self.interpret_operation(*op, params),
             AST::Identifier(ident) => {
                 self.push_data(self.resolve_identifier(&ident)?);
-                Ok(())
-            },
-            AST::Literal(_) => todo!(),
-            AST::List(head, tail) => todo!(),
-            AST::EmptyList => todo!(),
+            }
+            AST::Literal(lit) => {
+                self.push_data(ObjectPointer::Object(Object::Value(lit)));
+            }
+            AST::List(head, tail) => {
+                self.interpret(*head);
+                let ObjectPointer::Object(head) = self.pop_data()? else {
+                    panic!()
+                };
+
+                if *tail == AST::EmptyList {
+                    self.push_data(ObjectPointer::Object(Object::List(
+                        Box::new(head),
+                        Box::new(Object::Bottom),
+                    )))
+                } else {
+                    self.interpret(*tail)?;
+                    let ObjectPointer::Object(tail) = self.pop_data()? else {
+                        panic!()
+                    };
+                    self.push_data(ObjectPointer::Object(Object::List(
+                        Box::new(head),
+                        Box::new(tail),
+                    )));
+                }
+            }
+            AST::EmptyList => self.push_data(ObjectPointer::Object(Object::Bottom)),
         }
+        Ok(())
     }
 
     pub fn interpret_operation(&mut self, op: AST, mut body: Vec<AST>) -> InterpreterResult<()> {
@@ -49,33 +74,46 @@ impl InterpreterContext {
                     // Define a value
                     AST::Identifier(ident) => {
                         self.interpret(body.next().unwrap())?;
-                        let ObjectPointer::Object(object) = self.pop_data()? else { return Err(InterpreterError::ExpectedResult)};
+                        let ObjectPointer::Object(object) = self.pop_data()? else {
+                            return Err(InterpreterError::ExpectedResult);
+                        };
                         (ident, object)
-                    },
+                    }
                     // Define a function
                     AST::Operation(op_name, mut op_params) => {
-                        let AST::Identifier(op_name) = *op_name else { panic!("Expected Identifier {op_name}") };
+                        let AST::Identifier(op_name) = *op_name else {
+                            panic!("Expected Identifier {op_name}")
+                        };
 
-                        let param_names = Vec::new();
+                        let mut param_names = Vec::new();
                         for p in op_params.drain(..) {
                             match p {
                                 AST::Identifier(ident) => param_names.push(ident),
-                                e => panic!("Expected Identifier received {e}")
+                                e => panic!("Expected Identifier received {e}"),
                             }
                         }
 
-                        (op_name, Object::Func(Func::Defined(Some(op_name), param_names, body.next().unwrap())))
-                    },
+                        (
+                            op_name,
+                            Object::Func(Func::Defined(
+                                Some(op_name),
+                                param_names,
+                                body.next().unwrap(),
+                            )),
+                        )
+                    }
 
-                    e => return Err(InterpreterError::InvalidOperator(e))
+                    e => return Err(InterpreterError::InvalidOperator(e)),
                 };
                 assert!(body.len() == 0);
 
                 self.global_data.insert(ident, object);
-            },
+            }
             AST::Identifier(ident) => {
                 let pointer = self.resolve_identifier(&ident)?;
-                let Object::Func(func) = self.deref_pointer(&pointer)? else { return Err(InterpreterError::PointerIsNotFn) };
+                let Object::Func(func) = self.deref_pointer(&pointer)? else {
+                    return Err(InterpreterError::PointerIsNotFn);
+                };
 
                 match func {
                     Func::Macro(_, _) => todo!(),
@@ -95,35 +133,30 @@ impl InterpreterContext {
                 }
                 params.reverse();
 
-
                 self.frame_stack.push(value);
                 match func {
-                    Func::Native(_, f) => {
-                        f(self, param_count)
-                    },
-                    Func::Defined(_, _, _) => {
-
-                    },
+                    Func::Native(_, f) => f(self, param_count),
+                    Func::Defined(_, _, _) => {}
                 }
 
                 self.pop_frame()?;
             }
-            ast => return Err(InterpreterError::InvalidOperator(ast))
+            ast => return Err(InterpreterError::InvalidOperator(ast)),
         }
 
         Ok(())
     }
 
     // pub fn create_frame(&mut self, func: &Func, params: usize) -> InterpreterResult<()> {
-        // let frame = ;
-        // match func {
-        //     Func::Native(name, _) => {
+    // let frame = ;
+    // match func {
+    //     Func::Native(name, _) => {
 
-        //     },
-        //     Func::Defined(_, _, _) => {
+    //     },
+    //     Func::Defined(_, _, _) => {
 
-        //     },
-        // }
+    //     },
+    // }
     // }
 
     pub fn pop_frame(&mut self) -> InterpreterResult<()> {
@@ -139,7 +172,9 @@ impl InterpreterContext {
     }
 
     pub fn pop_data(&mut self) -> InterpreterResult<ObjectPointer> {
-        self.data_stack.pop().ok_or(InterpreterError::EmptyDataStack)
+        self.data_stack
+            .pop()
+            .ok_or(InterpreterError::EmptyDataStack)
     }
 
     pub fn resolve_identifier(&self, ident: &str) -> Result<ObjectPointer, InterpreterError> {
@@ -150,14 +185,19 @@ impl InterpreterContext {
             .rev()
             .find_map(|(i, frame)| frame.get_local(ident).map(|_| i))
         {
-            return Ok(ObjectPointer::Stack { frame_index: index, name: ident.to_string() })
+            return Ok(ObjectPointer::Stack {
+                frame_index: index,
+                name: ident.to_string(),
+            });
         }
 
         if self.global_data.get(ident).is_some() {
-            return Ok(ObjectPointer::Heap { name: ident.to_string() })
+            return Ok(ObjectPointer::Heap {
+                name: ident.to_string(),
+            });
         }
 
-        return Err(InterpreterError::InvalidIdentifier)
+        return Err(InterpreterError::InvalidIdentifier);
     }
 
     pub fn deref_pointer(&self, pointer: &ObjectPointer) -> Result<&Object, InterpreterError> {
